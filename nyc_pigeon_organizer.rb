8caeb8d41eac486f985a01026d2ba9b501ba284e
Maya Blevins <maya.blevins@gmail.com>
2020-05-05 14:58:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,30 @@
 
 def nyc_pigeon_organizer(data)
-  nyc_pigeon_organizer.keys
+new_hash = {}
+
+pigeon_data.each_pair do |key, key_hash|
+  key_hash.each_pair do |color_name, names_array|
+    names_array.each do |pigeon_name|
+      new_hash[pigeon_name] = {}
+    end
+  end
+end
+
+pigeon_data.each_pair do |key1, key_hash|
+  key_hash.each_pair do |key2, val|
+    val.each do |name|
+      new_hash[name][key1] = []
+      new_hash[name][key1] << key2.to_s 
+    end
+  end
+end
+
+pigeon_data[:color].each do |key, val|
+  val.each do |name|
+    if !new_hash[name][:color].include?(key.to_s)
+      new_hash[name][:color] << key.to_s
+    end
+  end
+end
+new_hash
 end
